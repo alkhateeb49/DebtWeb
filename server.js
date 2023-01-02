90bb8af7a2955430aa4bae6e68818879228d63fe
Mohammad Al Khatib <Mohammad.AlKhatib@fastlink.com.jo>
2023-01-02 17:04:49 +0300

    Superagent

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const superAgent = require('superagent');
-const override = require('method-override');
-const { query } = require('express');
-//
-dotenv.config();
-PORT=process.env.PORT;
+require('dotenv').config()
+
+const PORT=process.env.PORT || 8000;
+const superagent = require('superagent');
+let ejs = require('ejs');
+
 const app=express();
-app.use(cors());
-app.use(express.static('./public'));
-app.use(express.urlencoded({extended:true}));
-app.use(override('_method'));
-app.set('view engine','ejs');
+
 
 
 app.get('/',home); 
 function home(req,res){
-    let url='https://api.covid19api.com/world/total';
-    superAgent.get(url).then(data=>{
-        res.render('pages/index',{data:data.body});
+    let url='https://debtapi.onrender.com/show';
+    // superagent.get(url).then(data=>{
+    //     res.render('pages/index',{data:data.body});
+    // });
 
+    superagent.get(url).then(data=>{
+        res.status(200).send(data.body);
     });
 }
 
